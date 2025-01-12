d0fcd36c0659d74de8a0581c4acef4c8007ae17f
tai <taiseicee@gmail.com>
2025-01-12 14:21:38 -0500

    Thread locked the singleton

--- Ch_02_Starter/Assets/Scripts/Singleton.cs
+++ Ch_02_Starter/Assets/Scripts/Singleton.cs
@@ -2,18 +2,21 @@
 using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T: MonoBehaviour {
-    private static T s_instance;
     public static T Instance {
         get { return GetInstance(); }
     }
+    private static T s_instance;
+    private static readonly object s_threadLock = new();
+    private static bool s_isQuitting = false;
 
     private static T GetInstance() {
+        if (s_isQuitting) return null;
         if (s_instance != null) return s_instance;
 
         s_instance = FindAnyObjectByType<T>();
         if (s_instance != null) return s_instance;
 
-        CreateNewInstance();
+        lock (s_threadLock) { CreateNewInstance(); }
         return s_instance;
     }
     
@@ -27,5 +30,8 @@ public class Singleton<T> : MonoBehaviour where T: MonoBehaviour {
         DontDestroyOnLoad(s_instance);
         Debug.Log($"Manager Instance {s_instance.GetInstanceID()} Initialized");
     }
-    
+
+    private void OnDestroy() {
+        s_isQuitting = true;
+    }
 }
